f1edc80ab70c852d51cabf3d21468ccd3d9a747a
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-12-13 21:29:36 +0000

    passed connect() object to setupInput

--- play.js
+++ play.js
@@ -1,21 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log("Connecting ...");
-connect();
-//what happens when data is received from stdin
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-//setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+const connectionObj = connect();
 
-setupInput();
+setupInput(connectionObj);
