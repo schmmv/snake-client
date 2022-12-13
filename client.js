8d90e93d39bf247b02c8344cdad3a79af9685384
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-12-13 21:29:07 +0000

    Cleaned up the code, removed some comments

--- client.js
+++ client.js
@@ -8,17 +8,16 @@ const connect = function () {
   });
   
   conn.setEncoding('utf8'); //interpret incoming data as text
+
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: MMS");
-    // conn.write("Move: up");
-    // setInterval(() => {
-    //   conn.write("Move: up");
-    // }, 1000);
   });
+
   conn.on("data", () => {
     console.log("you ded cuz you idled");
-  })
+  });
+  
   return conn;
 };
 
