bb110bd3187bde0bbd1bf7b720aabd16d0588340
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-12-14 02:17:33 +0000

    Refactored to use constants from constants.js file

--- input.js
+++ input.js
@@ -1,4 +1,5 @@
 //setup interface to handle user input from stdin
+const { moves } = require('./constants');
 
 let connection;
 
@@ -13,19 +14,20 @@ const setupInput = function(conn) {
     if (key === '\u0003') {
       process.exit();
     }
-    if (key === 'w') {
-      connection.write("Move: up");
-    } else if (key === 'a') {
-      connection.write("Move: left");
-    } else if (key === 's') {
-      connection.write("Move: down");
-    } else if (key === 'd') {
-      connection.write("Move: right");
-    } else if (key === 'm') {
-      connection.write("Say: Outta my way!");
-    } else if (key === 'e') {
-      connection.write("Say: Eat my dust!");
-    }
+    connection.write(moves[key]);
+    // if (key === 'w') {
+    //   connection.write("Move: up");
+    // } else if (key === 'a') {
+    //   connection.write("Move: left");
+    // } else if (key === 's') {
+    //   connection.write("Move: down");
+    // } else if (key === 'd') {
+    //   connection.write("Move: right");
+    // } else if (key === 'm') {
+    //   connection.write("Say: Outta my way!");
+    // } else if (key === 'e') {
+    //   connection.write("Say: Eat my dust!");
+    // }
   });
   return stdin;
 };
